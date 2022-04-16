55ebb0e3ca88cdbb86378641041429f893b8e429
Bednarooo <236502@edu.p.lodz.pl>
2022-04-16 15:50:31 +0200

    Zadanie 3 - poprawki do wykresow

--- Zadanie 3/graphs.py
+++ Zadanie 3/graphs.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 def graph(a, b, arguments, values, file_arguments, calculated_file_values, i_arguments, i_values):
     plt.plot(arguments, values, color='r', label='Funkcja interpolowana')
     plt.plot(i_arguments, i_values, color='g', label='Funkcja interpolacyjna')
-    plt.scatter(file_arguments, calculated_file_values, c='b')
+    plt.scatter(file_arguments, calculated_file_values, c='b', label='Podane wezly')
     plt.legend()
+    plt.title("Wykresy funkcji interpolowanej i interpolacyjnej")
+    plt.xlabel("X")
+    plt.ylabel("Y")
     plt.xlim(a, b)
     plt.show()
